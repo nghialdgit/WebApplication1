c677d60412bd9212651fef22c675fc0cdc81f8dc
nghiald2909 <nghiald@softcom.vn>
2024-11-20 16:23:41 +0700

    fix layoutSidenav_nav

---
+++ WebApplication1/Controllers/RenderLayoutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RenderLayoutController : Controller
+    {
+        private List<MenuItem> MenuItems = new List<MenuItem>();
+        public RenderLayoutController()
+        {
+            MenuItems = new List<MenuItem>() {
+                new MenuItem() {Id=1, Name="Branches", Link="Branches/List" },
+                new MenuItem() {Id=2, Name="Students", Link="Students/List" },
+                new MenuItem() { Id=3, Name="Subjects", Link="Subjects/List"},
+                new MenuItem() { Id=4, Name="Courses", Link="Courses/List"}
+            };
+        }
+        public IActionResult RenderLeftMenu()
+        {
+            return PartialView("RenderLeftMenu", MenuItems);
+        }
+    }
+}
